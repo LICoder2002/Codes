cbe145d59c281d1e3947bcc54a85b1f1315a23e1
李海飞 <1136014334@qq.com>
2023-02-12 15:32:29 +0800

    配置Windows中vscode开发环境

---
+++ test_vscode/add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    printf("a + b + c = %d\n", a + b + c);
+    return 0;
+}
---
+++ test_vscode/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello vscode!\n");
+    return 0;
+}
