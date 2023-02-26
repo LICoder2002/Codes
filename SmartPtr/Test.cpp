ea75881673285e5b9fa4743fffbb6a48569d1411
李海飞 <1136014334@qq.com>
2023-02-26 10:46:55 +0800

    解决模拟实现的shared_ptr的线程安全问题

--- SmartPtr/Test.cpp
+++ SmartPtr/Test.cpp
@@ -3,6 +3,11 @@
 #include"unique_ptr.h"
 #include"shared_ptr.h"
 #include"weak_ptr.h"
+
+#include <thread>
+#include <vector>
+
+
 using namespace std;
 
 //int main()
@@ -76,24 +81,24 @@ using namespace std;
 //	return 0;
 //}
 
-
-struct ListNode
-{
-	/*ListNode* _next = nullptr;
-	ListNode* _prev = nullptr;*/
-
-
-
-	std::weak_ptr<ListNode> _next;
-	std::weak_ptr<ListNode> _prev;
-
-	int _val = 0;
-
-	~ListNode()
-	{
-		cout << "~ListNode()" << endl;
-	}
-};
+//
+//struct ListNode
+//{
+//	/*ListNode* _next = nullptr;
+//	ListNode* _prev = nullptr;*/
+//
+//
+//
+//	std::weak_ptr<ListNode> _next;
+//	std::weak_ptr<ListNode> _prev;
+//
+//	int _val = 0;
+//
+//	~ListNode()
+//	{
+//		cout << "~ListNode()" << endl;
+//	}
+//};
 //
 //int main()
 //{
@@ -110,32 +115,112 @@ struct ListNode
 //	cout << node2.use_count() << endl;
 //	return 0;
 //}
+//
+//template<class T>
+//struct FreeFunc
+//{
+//	void operator()(T* ptr)
+//	{
+//		cout << "free:" << ptr << endl;
+//		free(ptr);
+//	}
+//};
+//template<class T>
+//struct DeleteArrayFunc
+//{
+//	void operator()(T* ptr)
+//	{
+//		cout << "delete[]" << ptr << endl;
+//		delete[] ptr;
+//	}
+//};
+//int main()
+//{
+//	FreeFunc<int> freeFunc;
+//	std::shared_ptr<int> sp1((int*)malloc(4), freeFunc);
+//	DeleteArrayFunc<int> deleteArrayFunc;
+//
+//	std::shared_ptr<int> sp2((int*)malloc(4), deleteArrayFunc);
+//	//std::shared_ptr<FILE> sp5(fopen("test.txt", "w"), [](FILE* p) {fclose(p); });
+//	return 0;
+//}
 
-template<class T>
-struct FreeFunc
-{
-	void operator()(T* ptr)
-	{
-		cout << "free:" << ptr << endl;
-		free(ptr);
-	}
-};
-template<class T>
-struct DeleteArrayFunc
-{
-	void operator()(T* ptr)
-	{
-		cout << "delete[]" << ptr << endl;
-		delete[] ptr;
-	}
-};
+#include <memory>
+
+//int main()
+//{
+//	std::shared_ptr<double> sp1(new double(1.1));
+//	std::shared_ptr<double> sp2(sp1);
+//
+//	vector<thread> v(2);
+//	int n = 100;
+//	
+//	for (auto& t : v)
+//	{
+//		t = thread([&]() {
+//			for (int i = 0; i < n; ++i)
+//			{
+//				std::shared_ptr<double> sp(sp1);
+//
+//				(*sp)++;
+//			}
+//
+//
+//		});
+//	}
+//
+//	for (auto& t : v)
+//	{
+//		t.join();
+//	}
+//
+//	cout << sp1.use_count() << endl;
+//	cout << (*sp1) << endl;
+//
+//
+//	return 0;
+//}
+
+
+
 int main()
 {
-	FreeFunc<int> freeFunc;
-	std::shared_ptr<int> sp1((int*)malloc(4), freeFunc);
-	DeleteArrayFunc<int> deleteArrayFunc;
+	lhf::shared_ptr<double> sp1(new double(1.1));
+	lhf::shared_ptr<double> sp2(sp1);
 
-	std::shared_ptr<int> sp2((int*)malloc(4), deleteArrayFunc);
-	//std::shared_ptr<FILE> sp5(fopen("test.txt", "w"), [](FILE* p) {fclose(p); });
+	mutex mtx;
+
+	vector<thread> v(2);
+	int n = 100;
+
+	for (auto& t : v)
+	{
+		t = thread([&]() {
+			for (int i = 0; i < n; ++i)
+			{
+				//拷贝构造是线程安全的
+				//lhf::shared_ptr<double> sp(sp1);
+				lhf::shared_ptr<double> sp(new double(0.1));
+
+				//访问资源不是线程安全的
+				mtx.lock();
+				(*sp)++;
+				mtx.unlock();
+				//cout << "count: " << sp.use_count() << endl;
+			}
+
+
+			});
+	}
+
+	for (auto& t : v)
+	{
+		t.join();
+	}
+
+	cout << sp1.use_count() << endl;
+	cout << (*sp1) << endl;
+
+
 	return 0;
 }
--- SmartPtr/shared_ptr.h
+++ SmartPtr/shared_ptr.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <mutex>
+
 namespace lhf
 {
 	template<class T>
@@ -7,26 +10,43 @@ namespace lhf
 	private:
 		void release()
 		{
+			bool flag = false; //用于最后是否释放锁的标志
+
+			_pMutex->lock();
 			if (--(*_pCount) == 0 && _ptr)
 			{
+				std::cout << "delete" << std::endl;
 				delete _ptr;
 				_ptr = nullptr;
 
 				delete _pCount;
 				_pCount = nullptr;
+
+				flag = true;
+			}
+			_pMutex->unlock();
+
+			if (flag)
+			{
+				delete _pMutex;
+				_pMutex = nullptr;
 			}
 		}
 	public:
 		shared_ptr(T* ptr)
 			:_ptr(ptr)
 			,_pCount(new int(1))
+			,_pMutex(new std::mutex)
 		{}
 
 		shared_ptr(const shared_ptr<T>& p)
 			:_ptr(p._ptr)
 			, _pCount(p._pCount)
+			,_pMutex(p._pMutex)
 		{
-			++(* _pCount);
+			_pMutex->lock();
+			++(*_pCount);
+			_pMutex->unlock();
 		}
 
 		shared_ptr<T>& operator=(const shared_ptr<T>& p)
@@ -36,7 +56,11 @@ namespace lhf
 				this->release();
 				_ptr = p._ptr;
 				_pCount = p._pCount;
+				_pMutex = p._pMutex;
+
+				_pMutex->lock();
 				++(*_pCount);
+				_pMutex->unlock();
 			}
 			return *this;
 		}
@@ -55,9 +79,15 @@ namespace lhf
 			return _ptr;
 		}
 
+		int use_count()
+		{
+			return *_pCount;
+		}
+
 
 	private:
 		T* _ptr;
 		int* _pCount;
+		std::mutex* _pMutex;
 	};
 }
